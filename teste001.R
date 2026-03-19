e1a25177f1b006198e5980a084d8abb24e1532a2
Caio Queiroz <caioq8421@gmail.com>
2026-03-19 14:27:50 -0300

    Teste

--- teste001.R
+++ teste001.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 
 
-nome <- c('Caio', 'Júlia', 'Lucas', 'Luiza')
-idade <- c(18, 20, 16, 16)
+nome <- c('Caio', 'Júlia', 'Lucas', 'Luiza', 'Carla')
+idade <- c(18, 20, 16, 16, 42)
 dados <- data.frame(cbind(nome, idade))
 ggplot(data = dados, aes(x = nome, y = idade, group='idade', color = idade)) + geom_line() + geom_point()
 
