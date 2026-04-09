13898982c2928d78a999b2308fae777b67769df3
Caio Queiroz <caioq8421@gmail.com>
2026-04-09 12:50:34 -0300

    Atualizações

--- aulas/teste001.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-
-
-nome <- c('Caio', 'Júlia', 'Lucas', 'Luiza', 'Carla')
-idade <- c(18, 20, 16, 16, 42)
-dados <- data.frame(cbind(nome, idade))
-ggplot(data = dados, aes(x = nome, y = idade, group='idade', color = idade)) + geom_line() + geom_point()
-
